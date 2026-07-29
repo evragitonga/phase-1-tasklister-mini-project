ddef4a8adaf336fcd120970bf57adbeba700c382
Evra Gitonga <gitongawork@gmail.com>
2026-07-29 18:42:51 +0300

    Lab completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.querySelector('#create-task-form')
+  form.addEventListener('submit', (event) => {
+    event.preventDefault()
+    let formInput = document.querySelector('#new-task-description')
+    formInput.value = 'Wash the dishes' 
+    function buildToDo(){
+      let li = document.createElement('li')
+      li.textContent = formInput.value
+      let taskList = document.querySelector('#tasks')
+      taskList.append(li)
+    }
+    buildToDo()
+  })
 });
